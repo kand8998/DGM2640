721fa29dbb6222a987fa631e096b12f880caa4d6
Kaitlyn <kand8998@gmail.com>
2020-01-07 18:42:49 -0700

    Character Base and Game Action Scripts Added

---
+++ Character Scripting/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public abstract class CharacterBase : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float gravity = 3f;
+    public float jumpSpeed = 10;
+    public CharacterController Controller { get; set;}
+    protected Vector3 Location;
+    protected Vector3 Orientation;
+
+    public virtual void Move()
+    {
+        Location.x = Input.GetAxis("Horizontal")*moveSpeed;
+        Location.y += gravity * Time.deltaTime;
+        Controller.Move(Location * Time.deltaTime);
+    }
+
+    public virtual void Jump()
+    {
+        Location.y = jumpSpeed;
+    }
+    
+    public virtual void Grounded()
+    {
+        if (Controller.isGrounded)
+        {
+            Location.x = Input.GetAxis("Horizontal")*moveSpeed;
+        }
+    }
+    
+    public virtual void SwapCharacterClass(CharacterBase character)
+    {
+        //work
+    }
+}
