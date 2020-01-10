ea0b411b4a2c103e7488ce14a8766b63ff186710
temporary <kand8998@gmail.com>
2020-01-10 11:08:01 -0700

    Character controller based scripts
    using actions to change the radius, test grounded, and offset step

--- Character Scripting/Assets/Scripts/CharacterBehavior.cs
+++ Character Scripting/Assets/Scripts/CharacterBehavior.cs
@@ -7,24 +7,42 @@ public class CharacterBehavior : MonoBehaviour
     private Vector3 position;
     private CharacterController controller;
     
-    public float controllerHeight, controllerRadius;
-    public GameAction heightAction;
-    public GameAction groundedAction;
+    public float controllerHeight = 3f, controllerRadius = 2f, controllerOffset = 1f;
+    public GameAction heightAction, radiusAction, groundedAction, offsetAction;
+    public UnityEvent groundedEvent;
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
         heightAction.action += SetControllerHeight;
+        radiusAction.action += SetControllerRadius;
+        groundedAction.action += GroundedController;
+        offsetAction.action += ControllerStepOffset;
     }
     
-
     private void SetControllerHeight()
     {
         controller.height = controllerHeight;
     }
     
-    private void Boogers()
+    private void SetControllerRadius()
     {
         controller.radius = controllerRadius;
     }
+
+    private void GroundedController()
+    {
+        if (controller.isGrounded)
+        {
+            groundedEvent.Invoke();
+        }
+    }
+
+    private void ControllerStepOffset()
+    {
+        controller.stepOffset = controllerOffset;
+    }
+    
+    
+
 }
