dbbf5e8a5261be11d18c957bfe1cd45c5d898a3f
Kaitlyn <kand8998@gmail.com>
2020-01-09 17:59:09 -0700

    Movement Script Added

---
+++ Character Scripting/Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+    private Vector3 position;
+    private CharacterController controller;
+    
+    public float moveSpeed = 10f,  gravity = 9.81f, jumpSpeed = 30f;
+    private int jumpCount;
+    public int jumpCountMax = 2;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    private void Update()
+    {
+        position.x = moveSpeed*Input.GetAxis("Horizontal");
+        position.z = moveSpeed*Input.GetAxis("Vertical");
+        position.y -= gravity;
+
+        if (controller.isGrounded)
+        {
+            position.y = 0;
+            jumpCount = 0;
+        }
+        
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            position.y = jumpSpeed;
+            jumpCount++;
+        }
+        
+        controller.Move(position*Time.deltaTime);
+    }
+}
