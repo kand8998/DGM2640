8cca75099c3bd0c96195d8590dbc6364efb1047a
Kaitlyn <kand8998@gmail.com>
2020-01-13 13:35:11 -0700

    Scripts for Character Controller

--- Character Scripting/Assets/Scripts/CharacterBehavior.cs
+++ Character Scripting/Assets/Scripts/CharacterBehavior.cs
@@ -4,27 +4,32 @@ using UnityEngine.Events;
 [RequireComponent(typeof(CharacterController))]
 public class CharacterBehavior : MonoBehaviour
 {
-    private Vector3 position;
+    private Vector3 positionChange, controllerCenter, moveDirection = Vector3.zero;
     private CharacterController controller;
-    
-    public float controllerHeight = 3f, controllerRadius = 2f, controllerOffset = 1f;
-    public GameAction heightAction, radiusAction, groundedAction, offsetAction;
-    public UnityEvent groundedEvent;
+
+    public float controllerHeight = 3f, controllerRadius = 2f, controllerOffset = 1f,
+        speed = 6.0f, gravity = 20.0f;
+
+    public GameAction heightAction, radiusAction, groundedAction,
+        offsetAction, centerAction;
+
+    public UnityEvent groundedEvent, detectEvent, isTriggerEvent;
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
-        heightAction.action += SetControllerHeight;
+        heightAction.action = SetControllerHeight;
         radiusAction.action += SetControllerRadius;
         groundedAction.action += GroundedController;
         offsetAction.action += ControllerStepOffset;
+        centerAction.action += ControllerCenterChange;
     }
-    
+
     private void SetControllerHeight()
     {
         controller.height = controllerHeight;
     }
-    
+
     private void SetControllerRadius()
     {
         controller.radius = controllerRadius;
@@ -35,6 +40,9 @@ public class CharacterBehavior : MonoBehaviour
         if (controller.isGrounded)
         {
             groundedEvent.Invoke();
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
         }
     }
 
@@ -42,7 +50,35 @@ public class CharacterBehavior : MonoBehaviour
     {
         controller.stepOffset = controllerOffset;
     }
-    
-    
 
+    private void ControllerMove()
+    {
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void ControllerHashCode()
+    {
+        var hashCode = controller.GetHashCode();
+    }
+
+    private void ControllerCenterChange()
+    {
+        controller.center = controllerCenter;
+    }
+
+    private void ControllerBool()
+    {
+        if (controller.detectCollisions)
+        {
+            detectEvent.Invoke();
+        }
+
+        else if (controller.isTrigger)
+        {
+            isTriggerEvent.Invoke();
+        }
+    }
 }
+
+
