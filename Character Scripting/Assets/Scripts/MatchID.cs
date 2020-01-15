d091c9eff5e131fc2158005a682691f7cd49b9f7
Kaitlyn <kand8998@gmail.com>
2020-01-14 18:23:14 -0700

    ID matching script set

--- Character Scripting/Assets/Scripts/MatchID.cs
+++ Character Scripting/Assets/Scripts/MatchID.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MatchID : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public NameID nameIDObj;
+    
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var nameIdObj = other.GetComponent<MatchID>().nameIDObj;
+        if (nameIdObj == null) return;
+        var otherNameId = nameIdObj;
+        if (nameIDObj == otherNameId)
+        {
+            //do work
+        }
     }
 }
---
+++ Character Scripting/Assets/Scripts/TriggerEvents.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class TriggerEvents : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    public UnityEvent triggerEnterEvent, triggerExitEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
+    }
+}
