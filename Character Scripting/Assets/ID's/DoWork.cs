422acd772158b886d1646029e637d2b567ce138f
Kaitlyn <kand8998@gmail.com>
2020-01-16 13:07:09 -0700

    Match Id Scripts

---
+++ Character Scripting/Assets/ID's/DoWork.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DoWork : MonoBehaviour
+{
+    public NameID nameIDObj;
+
+    public void Work()
+    {
+        print("Working");
+    }
+}
---
+++ Character Scripting/Assets/ID's/MatchID.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MatchID : MonoBehaviour
+{
+    public NameID nameIdObj;
+    public IDCollection nameIdList;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        var doWorkObj = other.GetComponent<DoWork>().nameIDObj;
+        var otherNameId = doWorkObj.nameIDObj;
+        
+        foreach (var nameID in nameIdList)
+        {
+            // do work
+        }
+
+        if (nameIdObj == other.GetComponent<DoWork>().nameIDObj)
+        {
+           
+        }
+    }
+}
