9ae3c0d6f657a6de09c7ab5fd4c2fa1c2a1639e2
Kaitlyn <kand8998@gmail.com>
2020-01-21 17:57:50 -0700

    Animation behavior Exploring

--- Character Scripting/Assets/Scripts/AnimController.cs
+++ Character Scripting/Assets/Scripts/AnimController.cs
@@ -3,8 +3,15 @@
 [CreateAssetMenu]
 public class AnimController : ScriptableObject
 {
+    public float animationSpeed = 10f, animatorPlayback = 3f;
     public void AnimCall(Animator animator)
     {
         animator.SetTrigger(name);
+    }
+
+    public void AnimationFloats(Animator animator)
+    {
+        animator.speed = animationSpeed;
+        animator.playbackTime = animatorPlayback;
     }
 }
