43786065e41e273667ec650b64fe44533473a806
Kaitlyn <kand8998@gmail.com>
2020-01-21 18:34:41 -0700

    Scripting for the day

---
+++ Character Scripting/Assets/Scripts/LightData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class LightData : ScriptableObject
+{
+    public Light lightObj;
+
+    public void ColorChange()
+    {
+        lightObj.color = Color.cyan;
+    }
+}
---
+++ Character Scripting/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent;
+
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+}
