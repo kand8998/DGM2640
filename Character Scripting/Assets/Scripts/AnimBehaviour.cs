19b6c6d9460511bee25c79d1aa50c94b39e0c20f
Kaitlynn <kand8998@gmail.com>
2020-01-23 12:55:54 -0700

    Anim State and Skeleton added

---
+++ Character Scripting/Assets/Scripts/AnimBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimBehaviour : StateMachineBehaviour
+{
+    public UnityEvent stateEnterEvent;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        stateEnterEvent.Invoke();
+    }
+}
