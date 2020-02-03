a8b8336f9ea3e7b715e16d5da763125135812aea
Kaitlyn <kand8998@gmail.com>
2020-02-03 13:32:27 -0700

    coroutine  and mono events
    initial coroutine script based on instructor's

---
+++ Character Scripting/Assets/Scripts/CoroutineBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineBehaviour : MonoBehaviour
+{
+    public UnityEvent onEnableEvent, coroutineEvent, endCountDownEvent;
+    public float startHoldTime = 0;
+    public float repeatHoldTime = 1;
+    public int countDownNum;
+    private bool CanRun { get; set; }
+
+    private void OnEnable()
+    {
+        onEnableEvent.Invoke();
+    }
+
+    public void OnStartCoroutine()
+    {
+        StartCoroutine( RunCoroutine() );
+    }
+     
+    private IEnumerator RunCoroutine()
+    {
+        CanRun = true;
+        yield return new WaitForSeconds(startHoldTime);
+        
+        while (CanRun)
+        {
+            coroutineEvent.Invoke();
+            yield return new WaitForSeconds(repeatHoldTime);
+        } 
+    }
+    
+    public void CountDown()
+    {
+        countDownNum--;
+        if (countDownNum <= 0)
+        {
+            endCountDownEvent.Invoke();
+        }
+    }
+
+    public void ResetCountDownNum(int num)
+    {
+        countDownNum = num;
+    }
+}
---
+++ Character Scripting/Assets/Scripts/MonoEvents.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEvents : MonoBehaviour
+{
+    public UnityEvent startEvent, updateEvent, awakeEvent;
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+    
+    private void Update()
+    {
+        updateEvent.Invoke();
+    }
+
+    private void OnAwake()
+    {
+        awakeEvent.Invoke();
+    }
+}
