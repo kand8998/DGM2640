1ac966b15027bb8b6c91cdfb3625ffbad4ae3403
Kaitlyn <kand8998@gmail.com>
2020-02-05 16:28:54 -0700

    Patrol Script Written

---
+++ Character Scripting/Assets/Scripts/Patrol.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+    private void Start()
+    {
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position,
+            speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            randomSpot = Random.Range(0, moveSpots.Length);
+            waitTime = startWaitTime;
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+    }
+}
