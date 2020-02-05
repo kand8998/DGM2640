9eec4df467febe690031ad5a1ff0bda82baeb370
Kaitlyn <kand8998@gmail.com>
2020-02-05 15:10:11 -0700

    Follow a path script

---
+++ Character Scripting/Assets/Scripts/PathFollow.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PathFollow : MonoBehaviour
+{
+    private GameObject pathObj;
+    private Transform targetPath;
+    private int pathIndex = 0;
+    private float speed = 5;
+    
+    private void Start()
+    {
+        pathObj = GameObject.Find("Path");
+    }
+    
+    private void Update()
+    {
+        if (targetPath == null)
+        {
+            GetNextPathNode();
+        }
+
+        var direction = targetPath.position - this.transform.localPosition;
+        var distThisFrame = speed * Time.deltaTime;
+
+        if (direction.magnitude <= distThisFrame)
+        {
+            targetPath = null;
+        }
+        else
+        {
+            transform.Translate(direction.normalized * distThisFrame, Space.World);
+            var targetRotation = Quaternion.LookRotation(direction);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime*5);
+        }
+    }
+    
+    private void GetNextPathNode()
+    {
+        targetPath = pathObj.transform.GetChild(pathIndex);
+        pathIndex++;
+    }
+
+}
