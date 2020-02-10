79f7f09e4bbf3321f129b6ac91a3195da5184258
Kaitlyn <kand8998@gmail.com>
2020-02-10 16:29:44 -0700

    Character looking the direction of movement

--- Character Scripting/Assets/Scripts/Movement.cs
+++ Character Scripting/Assets/Scripts/Movement.cs
@@ -6,7 +6,9 @@ public class Movement : MonoBehaviour
     private Vector3 position;
     private CharacterController controller;
     
-    public float moveSpeed = 10f,  gravity = 9.81f, jumpSpeed = 30f;
+    public float moveSpeed = 10f, jumpSpeed = 30f, rotationSpeed = 100;
+    public float gravity = 9.81f;
+    
     private int jumpCount;
     public int jumpCountMax = 2;
 
@@ -20,21 +22,15 @@ public class Movement : MonoBehaviour
         position.x = moveSpeed*Input.GetAxis("Horizontal");
         position.z = moveSpeed*Input.GetAxis("Vertical");
         position.y -= gravity;
-
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            moveSpeed++;
-        }
-
-        if (Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            moveSpeed--;
-        }
         
         if (controller.isGrounded)
         {
             position.y = 0;
             jumpCount = 0;
+            if (position  != Vector3.zero)
+            {
+                controller.transform.forward = position;
+            }
         }
         
         if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
