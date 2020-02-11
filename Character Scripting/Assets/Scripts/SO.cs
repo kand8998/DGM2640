91c1584eba2985c3e9069cc2fb5e14fc93d6f5c7
Kaitlyn <kand8998@gmail.com>
2020-02-10 18:11:22 -0700

    Debug Script Separated

--- Character Scripting/Assets/Scripts/SO.cs
+++ Character Scripting/Assets/Scripts/SO.cs
@@ -41,36 +41,6 @@ public class AnimController : ScriptableObject
     }
 }
 
-[CreateAssetMenu]
-public class Debugger : ScriptableObject
-{
-    
-    public void OnDebug(string message)
-    {
-        Debug.Log(message);
-    }
-
-    public void OnDebug(int number)
-    {
-        Debug.Log(number);
-    }
-
-    public void OnDebug(float amount)
-    {
-        Debug.Log(amount);
-    }
-
-    public void OnDebug(bool answer)
-    {
-        Debug.Log(answer);
-    }
-
-    public void OnDebug(FloatData value)
-    {
-        Debug.Log(value);
-    }
-}
-
 [CreateAssetMenu(menuName = "Single Variables/FloatData")]
 public class FloatData : ScriptableObject
 {
