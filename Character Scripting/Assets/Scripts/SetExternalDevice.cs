bff3440f264c29d138ee7b8b267ae700b026a7b3
Kaitlyn <kand8998@gmail.com>
2020-02-12 11:09:58 -0700

    Testing Controller Scripts

---
+++ Character Scripting/Assets/Scripts/SetExternalDevice.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+[CreateAssetMenu]
+public class SetExternalDevice : ScriptableObject
+{
+    private void Tracking()
+    {
+        InputDevices.GetDeviceAtXRNode(XRNode.TrackingReference);
+    }
+
+    private void Sample()
+    {
+        InputDevices.GetDeviceAtXRNode(XRNode.GameController);
+    }
+}
