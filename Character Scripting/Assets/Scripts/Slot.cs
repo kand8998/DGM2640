27a8215ae24a834e9bb83793e28e3f54f060df64
Kaitlyn <kand8998@gmail.com>
2020-02-14 09:35:01 -0700

    Slot Script Added
    initial script based on a tutorial

---
+++ Character Scripting/Assets/Scripts/Slot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+  private Inventory inventory;
+  public int i;
+  public GameAction dropAction;
+
+  private void Start()
+  {
+    inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    dropAction.action += DropItem;
+  }
+
+  private void Update()
+  {
+    if (transform.childCount <= 0)
+    {
+      inventory.isFull[i] = false;
+    }
+  }
+
+  private void DropItem()
+  {
+    foreach (Transform child in transform)
+    {
+      child.GetComponent<Spawn>().SpawnDroppedItem();
+      GameObject.Destroy(child.gameObject);
+    }
+  }
+}
---
+++ Character Scripting/Assets/Scripts/Spawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+  public GameObject item;
+  private Transform player;
+  public GameAction droppedAction;
+
+  private void Start()
+  {
+    player = GameObject.FindGameObjectWithTag("Player").transform;
+    droppedAction.action += SpawnDroppedItem;
+  }
+
+  public void SpawnDroppedItem()
+  {
+    var playerPos = new Vector3(player.position.x, player.position.y+3, 0);
+    Instantiate(item, playerPos, Quaternion.identity);
+  }
+}
