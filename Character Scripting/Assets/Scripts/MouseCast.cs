9d6f5db0a1717659bc55654c3dc0be303b677234
Kaitlyn <kand8998@gmail.com>
2020-02-17 09:15:12 -0700

    Ray Casting on Mouse Click Script.

---
+++ Character Scripting/Assets/Scripts/MouseCast.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MouseCast : Movement
+{
+    private Camera cam;
+    
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+   
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray,out hit))
+            {
+                
+            }
+        }
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit,100))
+            {
+                
+            }
+        }
+    }
+}
---
+++ Character Scripting/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void MoveToPoint(Vector3 point)
+    {
+        agent.SetDestination(point);
+    }
+}
