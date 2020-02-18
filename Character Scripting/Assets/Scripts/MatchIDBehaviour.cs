1d91fa84648878abf7d9b49c6c70e888a0f2b7a1
Kaitlynn <kand8998@gmail.com>
2020-02-18 13:07:37 -0700

    Match Id behavior re-written.

--- Character Scripting/Assets/Scripts/MatchIDBehaviour.cs
+++ Character Scripting/Assets/Scripts/MatchIDBehaviour.cs
@@ -3,4 +3,21 @@
 public class MatchIDBehaviour : IDBehaviour
 {
     public WorkSystemManager workSystemMangerObj;
+    private NameID otherIDObj;
+    private void OnTriggerEnter(Collider other)
+    {
+        otherIDObj = other.GetComponent<IDBehaviour>().nameIdObj;
+        CheckId();
+    }
+
+    private void CheckId()
+    {
+        foreach (var obj in workSystemMangerObj.workIdList)
+        {
+            if (otherIDObj == obj.nameIdObj)
+            {
+                obj.workSystemObj.Work();
+            }
+        }
+    }
 }
