cf98a77ece0947e0df4409a203e4662bc2bb4bf7
Kaitlynn <kand8998@gmail.com>
2020-02-18 17:11:18 -0700

    Attack Script Started

---
+++ Character Scripting/Assets/Scripts/CharCombat.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class CharCombat : MonoBehaviour
+{
+    public float attackSpeed = 1f;
+    private float attackCoolDown = 0f; 
+    public float attackDelay = 0f;
+
+    public FloatData characterData;
+
+    public event System.Action OnAttack;
+    void Start()
+    {
+        characterData.value = 1f;
+    }
+
+    private void Update()
+    {
+        attackCoolDown -= Time.deltaTime;
+    }
+
+    public void Attack(FloatData targetStats)
+    {
+        if (attackCoolDown <= 0f)
+        {
+            StartCoroutine(DoDamage(targetStats, attackDelay ));
+            if (OnAttack != null)
+                OnAttack();
+            
+            attackCoolDown = 1f / attackSpeed;
+        }
+       
+    }
+
+    IEnumerator DoDamage(FloatData stats, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        stats.maxValue -= characterData.value;
+
+    }
+}
