b47307589ae964f186c1cf2cd5188971f043e6ea
Kaitlyn <kand8998@gmail.com>
2020-02-19 16:05:11 -0700

    Character Movement Collections Removed

--- Character Scripting/Assets/Scripts/CharacterMovement.cs
+++ Character Scripting/Assets/Scripts/CharacterMovement.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class CharacterMovement : MoveBase
