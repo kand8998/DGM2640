1a2599a55a924d4d7e773a56d085d038eb1dd370
Kaitlynn <kand8998@gmail.com>
2020-02-20 13:57:00 -0700

    Matching Systems

---
+++ Character Scripting/Assets/Scripts/ColorData.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class ColorData : ScriptableObject
+{
+    public Color value = Color.blue;
+}
---
+++ Character Scripting/Assets/Scripts/DebugSystem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class DebugSystem : WorkSystem
+{
+    public string message;
+    public override void Work()
+    {
+        Debug.Log(message);
+    }
+}
