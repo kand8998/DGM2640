2bc6e1ff98ea78ee53e2ba1a91347744b015b785
Kaitlynn <kand8998@gmail.com>
2020-02-20 17:00:28 -0700

    Drag Script Added

---
+++ Character Scripting/Assets/Scripts/DragBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DragBehaviour : MonoBehaviour
+{
+    private Vector3 offsetPosition;
+        private Vector3 newPosition;
+        private Camera cam;
+
+        public UnityEvent onDrag, onUp;
+        private bool CanDrag { get; set; }
+        private bool Draggable { get; set; }
+
+        private void Start()
+        {
+            cam = Camera.main;
+            Draggable = true;
+        }
+
+        public IEnumerator OnMouseDown()
+        {
+            onDrag.Invoke();
+            offsetPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
+            yield return new WaitForFixedUpdate();
+            CanDrag = true;
+            while (CanDrag)
+            {
+                yield return new WaitForFixedUpdate();
+                newPosition = cam.ScreenToWorldPoint(Input.mousePosition) + offsetPosition;
+                transform.position = newPosition;
+            }
+        }
+
+        private void OnMouseUp()
+        {
+            CanDrag = false;
+            if (Draggable)
+            {
+                onUp.Invoke();
+            }
+        }
+}
