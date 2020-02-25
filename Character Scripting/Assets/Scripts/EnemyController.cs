ea7f3809c6fdfc9c32084134eab9596ac4fcf57a
Kaitlyn <kand8998@gmail.com>
2020-02-24 17:53:24 -0700

    Enemy Basic Script Added

---
+++ Character Scripting/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    
+    public Transform target;
+    private NavMeshAgent agent;
+    
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = PlayerManager.instance.player.transform;
+    }
+
+    
+    private void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
