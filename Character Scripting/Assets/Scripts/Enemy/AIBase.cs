0e78304b38ea32ccfcfaf7fa87f1618aa004be7c
Kaitlynn <kand8998@gmail.com>
2020-03-03 15:57:55 -0700

    AI Basic Script Added

---
+++ Character Scripting/Assets/Scripts/Enemy/AIBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class AIBase : ScriptableObject
+{
+    public FloatData speed;
+    public FloatData angularSpeed;
+    public abstract IEnumerator Nav(NavMeshAgent ai);
+}
---
+++ Character Scripting/Assets/Scripts/Enemy/AIHunt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+
+public class AIHunt : AIBase
+{
+    public GameAction destinationAction;
+    public Transform destination;
+    
+    protected void Raise(object obj)
+    {
+        destination = obj as Transform;
+    }
+
+    public override IEnumerator Nav(NavMeshAgent ai)
+    {
+        while (true)
+        {
+            yield return new WaitForFixedUpdate();
+            ai.speed = speed.value;
+            ai.angularSpeed = angularSpeed.value;
+            ai.destination = (destination != null ? destination.position : ai.transform.position);
+        }
+    }
+}
