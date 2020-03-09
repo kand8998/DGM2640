8150d9647b9198676cfb4904da836f54cf97b714
Kaitlyn <kand8998@gmail.com>
2020-03-09 08:56:33 -0600

    Working on Character Stats Scripts

---
+++ Character Scripting/Assets/Scripts/Enemy/ZoneEnemy/CharStats.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+    public int maxHP = 100;
+    private int currentHP { get; set; }
+    public Stats damage;
+    public HealthBar healthBar;
+
+    private void Awake()
+    {
+        currentHP = maxHP;
+    }
+
+    private void Start()
+    {
+        if (healthBar != null) healthBar.SetMaxHealth(maxHP);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+        currentHP -= damage;
+        Debug.Log(transform.name + "takes" + damage + "damage.");
+
+        if (currentHP <= 0)
+        {
+            //
+        }
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + "died");
+    }
+}
---
+++ Character Scripting/Assets/Scripts/Enemy/ZoneEnemy/Stats.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Stats
+{
+    [SerializeField]private int baseValue;
+
+    public int GetValue()
+    {
+        int finalValue = baseValue;
+        return finalValue;
+    }
+    
+}
