87ab702c123b817368feb33218b2f214ebbdc20a
Kaitlynn <kand8998@gmail.com>
2020-03-10 16:14:07 -0600

    Other Collection and Data storage Scripts

---
+++ Character Scripting/Assets/Scripts/Collections/Item Collection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class ItemCollection : Collectable
+{
+    public UnityEvent useEvent;
+    public override void Use()
+    {
+        useEvent.Invoke();
+    }
+
+    public void Value()
+    {
+    }
+
+    public void Place()
+    {
+    }
+
+    public void Inventory()
+    {
+    }
+}
---
+++ Character Scripting/Assets/Scripts/OtherBehaviours/DataStorage.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "DataStorage", menuName = "Data Storage/Data Storage Object")]
+public class DataStorage : ScriptableObject
+{
+    public ScriptableObject data;
+
+    public void SetData()
+    {
+        if (data == null) return;
+        PlayerPrefs.SetString(data.name, JsonUtility.ToJson(data));
+    }
+
+    public void GetData()
+    {
+        if (data == null) return;
+        if (!string.IsNullOrEmpty(PlayerPrefs.GetString(data.name)))
+            JsonUtility.FromJsonOverwrite(PlayerPrefs.GetString(data.name), data);
+    }
+}
