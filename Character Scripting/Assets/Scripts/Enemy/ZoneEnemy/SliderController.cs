c3b989d513d55913a0da98c77098f33efcf1e297
Kaitlyn <kand8998@gmail.com>
2020-03-11 11:25:41 -0600

    Gradient Added to Slider
    Initially based on a tutorial

--- Character Scripting/Assets/Scripts/Enemy/ZoneEnemy/SliderController.cs
+++ Character Scripting/Assets/Scripts/Enemy/ZoneEnemy/SliderController.cs
@@ -5,15 +5,20 @@ using UnityEngine.UI;
 public class SliderController : MonoBehaviour
 {
     public Slider slider;
-
+    public Gradient gradient;
+    public Image fill;
     public void SetMaxHealth(int amount)
     {
         slider.maxValue = amount;
         slider.value = amount;
+
+        fill.color = gradient.Evaluate(1f);
+        
     }
 
     public void SetHealth(int amount)
     {
         slider.value = amount;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
