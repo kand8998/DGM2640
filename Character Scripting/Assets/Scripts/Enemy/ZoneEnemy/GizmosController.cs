a1e6ab9be21381027c17fec4bd7efcb0aeee68c2
Kaitlyn <kand8998@gmail.com>
2020-03-11 11:55:53 -0600

    Writing Scripts Using Gizmos

---
+++ Character Scripting/Assets/Scripts/Enemy/ZoneEnemy/GizmosController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GizmosController : MonoBehaviour
+{
+  public UnityEvent rangeEvent;
+  public float radius = 10.0f;
+  public Transform target;
+
+  private void OnDrawGizmosSelected()
+  {
+    Gizmos.color = Color.magenta;
+    Gizmos.DrawWireSphere(transform.position, radius);
+  }
+
+  private void Update()
+  {
+    float distance = Vector3.Distance(target.position, transform.position);
+    
+    if (distance < radius)
+    {
+      rangeEvent.Invoke();
+    }
+  }
+}
