baa0c94cd2748de483a3250fabc558967a95c3b0
Kaitlynn <kand8998@gmail.com>
2020-03-12 17:37:11 -0600

    String List Scripts Added

---
+++ Character Scripting/Assets/Scripts/UI/StringListManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class StringListManager : ScriptableObject
+{
+    public List<StringListData> stringListDatas;
+    public int currentListNum;
+
+    private StringListData ReturnCurrentData()
+    {
+        return stringListDatas[currentListNum];
+    }
+
+    private void IncrementListNum()
+    {
+        if (currentListNum < stringListDatas.Count - 1)
+        {
+            currentListNum++;
+        }
+        else
+        {
+            currentListNum = 0;
+        }
+    }
+
+    public void UseNextListOnEnd()
+    {
+        if (ReturnCurrentData().currentLineNumber == ReturnCurrentData().stringListObj.Count - 1 )
+        {
+            IncrementListNum();
+        }
+    }
+}
