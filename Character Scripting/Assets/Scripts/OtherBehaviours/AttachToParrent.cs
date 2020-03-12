dc649e6a559c8e5ccce649580b80986ba6b5d0ef
Kaitlynn <kand8998@gmail.com>
2020-03-12 17:38:25 -0600

    Attach to parent scirpts

---
+++ Character Scripting/Assets/Scripts/OtherBehaviours/AttachToParrent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AttachToParrent : MonoBehaviour
+{
+ 
+    public NameID parentId;
+  
+    private void OnTriggerEnter(Collider other)
+    {
+        var nameIdObj = other.GetComponent<IDBehaviour>().nameIdObj;
+      
+        if (nameIdObj != null && parentId == nameIdObj)
+        {
+            transform.SetParent(other.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        transform.parent = null;
+    }
+}
