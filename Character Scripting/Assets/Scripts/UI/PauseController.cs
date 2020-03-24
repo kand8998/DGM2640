3fa3d32105ba755c9e70e240edd727eb7e3c8d62
Kaitlyn <kand8998@gmail.com>
2020-03-24 17:34:13 -0600

    Pause Game Script Started

---
+++ Character Scripting/Assets/Scripts/UI/PauseController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PauseController : MonoBehaviour
+{
+    private static bool gameIsPaused = false;
+    public UnityEvent resumeEvent, pauseEvent;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    private void ResumeGame()
+    {
+        resumeEvent.Invoke();
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    private void PauseGame()
+    {
+        pauseEvent.Invoke();
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
