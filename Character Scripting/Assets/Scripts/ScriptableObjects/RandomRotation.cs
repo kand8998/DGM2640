b7ce8ea18e40ee4f07d033a5b741e98aee531367
Kaitlyn <kand8998@gmail.com>
2020-03-25 17:04:00 -0600

    Random Rotation Script Started

---
+++ Character Scripting/Assets/Scripts/ScriptableObjects/RandomRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+[CreateAssetMenu(menuName = "Tools/RotateValue")]
+public class RandomRotation : ScriptableObject
+{
+    private Vector3[] rotationsValues;
+    private int randomValue;
+
+    public void RandomRotationValue(GameObject obj)
+    {
+        randomValue = Random.Range(0, rotationsValues.Length);
+        
+    }
+}
