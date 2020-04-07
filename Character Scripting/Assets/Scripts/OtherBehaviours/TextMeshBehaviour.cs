41f67dcc834aa2f84c1940c860fac7fff8898cf7
Kaitlyn <kand8998@gmail.com>
2020-04-06 20:51:01 -0600

    Text Mesh Behaviour Script added to repository

---
+++ Character Scripting/Assets/Scripts/OtherBehaviours/TextMeshBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TextMeshBehaviour : MonoBehaviour
+{
+    private TextMeshProUGUI textObj;
+    public UnityEvent startEvent;
+  
+    private void Start()
+    {
+        textObj = GetComponent<TextMeshProUGUI>();
+        startEvent.Invoke();
+    }
+    
+    public void UpdateText(FloatData floatDataObj)
+    {
+        textObj.text = floatDataObj.value.ToString();
+    }
+}
