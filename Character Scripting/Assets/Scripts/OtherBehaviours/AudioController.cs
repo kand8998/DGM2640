ef009d64b2d7da05dd7792accc53a4ef86c620d9
Kaitlyn <kand8998@gmail.com>
2020-04-21 19:48:15 -0600

    Scripting

---
+++ Character Scripting/Assets/Scripts/OtherBehaviours/AudioController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour
+{
+    private AudioSource audioObj;
+    
+
+    private void Start()
+    {
+       audioObj = GetComponent<AudioSource>();
+    }
+
+    private void SetVolume(float amount)
+    {
+        audioObj.volume = amount;
+        
+    }
+
+    private void PlayAudio()
+    {
+        audioObj.Play();
+    }
+
+
+    private void SetClip(AudioClip clipObj)
+    {
+        audioObj.clip = clipObj;
+    }
+}
---
+++ Character Scripting/Assets/Scripts/OtherBehaviours/CursorObj.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class CursorObj : ScriptableObject
+{
+  private void ShowCursor()
+  {
+    Cursor.visible = true;
+  }
+
+  private void HideCursor()
+  {
+    Cursor.visible = false;
+  }
+}
